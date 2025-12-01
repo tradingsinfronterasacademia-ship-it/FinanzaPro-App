f0472fb9dad87cd2517bb37bba35d35eb1229050
tradingsinfronterasacademia-ship-it <tradingsinfronterasacademia@gmail.com>
2025-12-01 01:00:30 -0300

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
